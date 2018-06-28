9b92580a5dfb49b501eb88708b98d2232706f885
Jeff <zjiefan@gmail.com>
2018-06-27 21:01:32 -0400

    first commit

---
+++ fractual/main.py
@@ -0,0 +1,236 @@
+FREQ = 5
+DO_OST_TEST = True
+DO_VF_TEST = True
+
+PROB_TABLE = {}
+PROB_TABLE['no_ost_no_trt'] = {}
+PROB_TABLE['no_ost_no_trt']['no_fx'] = {}
+PROB_TABLE['no_ost_no_trt']['no_fx']['hip'] = 0.00070024
+PROB_TABLE['no_ost_no_trt']['no_fx']['vf']  = 0.00070024
+PROB_TABLE['no_ost_no_trt']['no_fx']['wf']  = 0.00006299
+PROB_TABLE['no_ost_no_trt']['hip'] = {}
+PROB_TABLE['no_ost_no_trt']['hip']['hip'] = 0.00786026
+PROB_TABLE['no_ost_no_trt']['hip']['vf']  = 0.00480247
+PROB_TABLE['no_ost_no_trt']['hip']['wf']  = 0.0025
+PROB_TABLE['no_ost_no_trt']['vf'] = {}
+PROB_TABLE['no_ost_no_trt']['vf']['hip'] = 0.00370072
+PROB_TABLE['no_ost_no_trt']['vf']['vf']  = 0.02480771
+PROB_TABLE['no_ost_no_trt']['vf']['wf']  = 0.00511307
+PROB_TABLE['no_ost_no_trt']['wf'] = {}
+PROB_TABLE['no_ost_no_trt']['wf']['hip'] = 0.00257979
+PROB_TABLE['no_ost_no_trt']['wf']['vf']  = 0.00220672
+PROB_TABLE['no_ost_no_trt']['wf']['wf']  = 0.00175901
+
+PROB_TABLE['has_ost_no_trt'] = {}
+PROB_TABLE['has_ost_no_trt']['no_fx'] = {}
+PROB_TABLE['has_ost_no_trt']['no_fx']['hip'] = 0.00277684
+PROB_TABLE['has_ost_no_trt']['no_fx']['vf']  = 0.00744093
+PROB_TABLE['has_ost_no_trt']['no_fx']['wf']  = 0.00599948
+PROB_TABLE['has_ost_no_trt']['hip'] = {}
+PROB_TABLE['has_ost_no_trt']['hip']['hip'] = 0.01786026
+PROB_TABLE['has_ost_no_trt']['hip']['vf']  = 0.00780247
+PROB_TABLE['has_ost_no_trt']['hip']['wf']  = 0.0055
+PROB_TABLE['has_ost_no_trt']['vf'] = {}
+PROB_TABLE['has_ost_no_trt']['vf']['hip'] = 0.00631997
+PROB_TABLE['has_ost_no_trt']['vf']['vf']  = 0.02672291
+PROB_TABLE['has_ost_no_trt']['vf']['wf']  = 0.00695305
+PROB_TABLE['has_ost_no_trt']['wf'] = {}
+PROB_TABLE['has_ost_no_trt']['wf']['hip'] = 0.00357979
+PROB_TABLE['has_ost_no_trt']['wf']['vf']  = 0.00320672
+PROB_TABLE['has_ost_no_trt']['wf']['wf']  = 0.00275901
+
+PROB_TABLE['has_ost_trt'] = {}
+PROB_TABLE['has_ost_trt']['no_fx'] = {}
+PROB_TABLE['has_ost_trt']['no_fx']['hip'] = 0.0012555
+PROB_TABLE['has_ost_trt']['no_fx']['vf']  = 0.00264944
+PROB_TABLE['has_ost_trt']['no_fx']['wf']  = 0.00534908
+PROB_TABLE['has_ost_trt']['hip'] = {}
+PROB_TABLE['has_ost_trt']['hip']['hip'] = 0.00520493
+PROB_TABLE['has_ost_trt']['hip']['vf']  = 0.00402953
+PROB_TABLE['has_ost_trt']['hip']['wf']  = 0.00292734
+PROB_TABLE['has_ost_trt']['vf'] = {}
+PROB_TABLE['has_ost_trt']['vf']['hip'] = 0.0018418
+PROB_TABLE['has_ost_trt']['vf']['vf']  = 0.01380084
+PROB_TABLE['has_ost_trt']['vf']['wf']  = 0.00370072
+PROB_TABLE['has_ost_trt']['wf'] = {}
+PROB_TABLE['has_ost_trt']['wf']['hip'] = 0.00104324
+PROB_TABLE['has_ost_trt']['wf']['vf']  = 0.00165609
+PROB_TABLE['has_ost_trt']['wf']['wf']  = 0.00146847
+
+PROB_TABLE['no_ost_trt'] = {}
+PROB_TABLE['no_ost_trt']['no_fx'] = {}
+PROB_TABLE['no_ost_trt']['no_fx']['hip'] = 0.000308106
+PROB_TABLE['no_ost_trt']['no_fx']['vf']  = 0.00039213
+PROB_TABLE['no_ost_trt']['no_fx']['wf']  = 0.00005543
+PROB_TABLE['no_ost_trt']['hip'] = {}
+PROB_TABLE['no_ost_trt']['hip']['hip'] = 0.00345851
+PROB_TABLE['no_ost_trt']['hip']['vf']  = 0.00268938
+PROB_TABLE['no_ost_trt']['hip']['wf']  = 0.0022
+PROB_TABLE['no_ost_trt']['vf'] = {}
+PROB_TABLE['no_ost_trt']['vf']['hip'] = 0.001628317
+PROB_TABLE['no_ost_trt']['vf']['vf']  = 0.013892318
+PROB_TABLE['no_ost_trt']['vf']['wf']  = 0.004499502
+PROB_TABLE['no_ost_trt']['wf'] = {}
+PROB_TABLE['no_ost_trt']['wf']['hip'] = 0.001135108
+PROB_TABLE['no_ost_trt']['wf']['vf']  = 0.001235763
+PROB_TABLE['no_ost_trt']['wf']['wf']  = 0.001547929
+
+PROB_TABLE['has_vf_no_trt'] = {}
+PROB_TABLE['has_vf_no_trt']['no_fx'] = {}
+PROB_TABLE['has_vf_no_trt']['no_fx']['hip'] = 0.01453593
+PROB_TABLE['has_vf_no_trt']['no_fx']['vf']  = 0.33670867
+PROB_TABLE['has_vf_no_trt']['no_fx']['wf']  = 0.0111249
+PROB_TABLE['has_vf_no_trt']['hip'] = {}
+PROB_TABLE['has_vf_no_trt']['hip']['hip'] = 0.018078598
+PROB_TABLE['has_vf_no_trt']['hip']['vf']  = 0.06051112
+PROB_TABLE['has_vf_no_trt']['hip']['wf']  = 0.004
+PROB_TABLE['has_vf_no_trt']['vf'] = {}
+PROB_TABLE['has_vf_no_trt']['vf']['hip'] = 0.00631997
+PROB_TABLE['has_vf_no_trt']['vf']['vf']  = 0.02672291
+PROB_TABLE['has_vf_no_trt']['vf']['wf']  = 0.00695305
+PROB_TABLE['has_vf_no_trt']['wf'] = {}
+PROB_TABLE['has_vf_no_trt']['wf']['hip'] = 0.00593352
+PROB_TABLE['has_vf_no_trt']['wf']['vf']  = 0.02780467
+PROB_TABLE['has_vf_no_trt']['wf']['wf']  = 0.00281442
+
+PROB_TABLE['has_vf_trt'] = {}
+PROB_TABLE['has_vf_trt']['no_fx'] = {}
+PROB_TABLE['has_vf_trt']['no_fx']['hip'] = 0.00639581
+PROB_TABLE['has_vf_trt']['no_fx']['vf']  = 0.18855685
+PROB_TABLE['has_vf_trt']['no_fx']['wf']  = 0.00978989
+PROB_TABLE['has_vf_trt']['hip'] = {}
+PROB_TABLE['has_vf_trt']['hip']['hip'] = 0.00795458
+PROB_TABLE['has_vf_trt']['hip']['vf']  = 0.03388623
+PROB_TABLE['has_vf_trt']['hip']['wf']  = 0.00352
+PROB_TABLE['has_vf_trt']['vf'] = {}
+PROB_TABLE['has_vf_trt']['vf']['hip'] = 0.0018418
+PROB_TABLE['has_vf_trt']['vf']['vf']  = 0.0138084
+PROB_TABLE['has_vf_trt']['vf']['wf']  = 0.00370072
+PROB_TABLE['has_vf_trt']['wf'] = {}
+PROB_TABLE['has_vf_trt']['wf']['hip'] = 0.00261075
+PROB_TABLE['has_vf_trt']['wf']['vf']  = 0.01557062
+PROB_TABLE['has_vf_trt']['wf']['wf']  = 0.00247669
+
+
+
+self to_status_str(has_ost, has_vf, trt):
+    if (not ost) and (not vf) and (not trt):
+        return 'no_ost_no_trt'
+    elif (not ost) and (not vf) and trt:
+        return 'no_ost_trt'
+    elif (not ost) and vf and (not trt):
+        return 'vf_no_trt'
+    elif (not ost) and vf and trt:
+        return 'vf_trt'
+    elif ost and (not vf) and (not trt):
+        return 'ost_no_trt'
+    elif ost and (not vf) and trt:
+        return 'ost_trt'
+    elif ost and vf and (not trt):
+        return 'vf_no_trt'
+    elif ost and vf and trt:
+        return 'vf_trt'
+
+
+VF_SENSI = 0.85
+VF_SPEC = 0.92
+VF_PREVAL = {}
+VF_PREVAL[50] = 0.017
+VF_PREVAL[60] = 0.0386
+VF_PREVAL[70] = 0.0647
+
+
+OST_SENSI = 0.73
+OST_SPEC = 1
+OST_PREVAL = {}
+OST_PREVAL[55] = 0.561
+OST_PREVAL[60] = 0.657
+OST_PREVAL[70] = 0.775
+OST_PREVAL[80] = 0.876
+
+def ost_preval(age):
+    if age < 60:
+        return OST_PREVAL[55]
+    elif age < 70:
+        return OST_PREVAL[60]
+    elif age < 80:
+        return OST_PREVAL[70]
+    else:
+        return OST_PREVAL[80]
+
+class Human(object):
+    def __init__(self):
+        self.age = 55
+        self.next_test_age = self.age + FREQ
+        self.utils = 0
+        self.cost = 0
+        self.ost = False        
+        if random.random() < ost_preval(self.age):
+            self.ost = True
+        self.fx = 'no_fx'
+        if random.random() < preval(self.age):
+            self.fx = 'vf'
+        self.trt = None
+        self.ost_test = None
+        self.vf_test = None
+    
+    def lab_test(self):
+        self.trt = None
+        self.ost_test = None
+        self.vf_test = None
+        # if there is fx, ost is test is always positive, and therefore get treatment
+        if self.fx != 'no_fx':
+            self.ost_test = 'Pos'
+            self.trt = True
+        # if no fracture.         
+        elif DO_OST_TEST:
+            if self.has_ost:
+                #  if has ost, OST_SENSI chance positve test result. 
+                if random.random() < OST_SENSI: 
+                    self.ost_test = 'Pos'
+                    self.trt = True
+                else:
+                    self.ost_test = 'Neg'
+                    self.trt = False
+            else:
+                if random.random() > OST_SPEC:
+                    self.ost_test = 'Pos'
+                    self.trt = True
+                else:
+                    self.ost_test = 'Neg'
+                    self.trt = False
+            
+            if DO_VF_TEST and self.ost_test == 'Neg':
+                if self.fx == 'vf':
+                    if random.random() < VF_SENSI: 
+                        self.vf_test = 'Pos'
+                        self.trt = True
+                    else:
+                        self.vf_test = 'Neg'
+                        self.trt = False                        
+                else:
+                    if random.random() > VF_SPEC:
+                        self.vf_test = 'Pos'
+                        self.trt = True
+                    else:
+                        self.vf_test = 'Neg'
+                        self.trt = False              
+            
+        if self.trt is None:
+            raise Exception("trt cannot be None")
+        if DO_OST_TEST and (self.ost_test is None):
+            raise Exception("everyone should have done OST TEST")
+        
+
+    def next_cycle(self):
+        self.age += 0.5
+        has_vf = self.fx == 'vf'
+        status_str = to_status_str(self.ost, has_vf, self.trt)
+        
+        
+
+
+
+
+    
+    
