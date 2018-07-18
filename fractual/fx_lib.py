b63bfe6bcf4d9d09a88893b1e19988b2bf521d18
jeff <jizhang@host1.jznet>
2018-07-17 22:57:10 -0400

    WIP

--- fractual/fx_lib.py
+++ fractual/fx_lib.py
@@ -109,8 +109,8 @@ def get_utility(fx, age):
 
 
 class Human(object):
-    __slots__ = ('test_freq', 'do_ost_test', 'do_vf_test',
-    'age', 'next_test', 'acc_utils', 'acc_cost', 'ost', 'vfa','fx', 'trt', 'ost_test', 'vfa_test',
+    __slots__ = ('test_freq', 'strategy',
+    'age', 'next_test', 'acc_utils', 'acc_cost', 'ost', 'vfa','fx', 'trt', 'ost_result', 'vfa_result',
     'vf_cnt', 'hip_cnt', 'wf_cnt', 'trt_cnt',
     'drug_end', 'drug_holiday_end',
     'last_hip', 'last_vf', 'last_wf', 'fx_rate',
@@ -119,10 +119,9 @@ class Human(object):
     'discount'
 
     )
-    def __init__(self, base_age=None, test_freq=None, do_ost_test=None, do_vf_test=None):
+    def __init__(self, base_age=None, test_freq=None, strategy=None):
         self.test_freq = test_freq
-        self.do_ost_test = do_ost_test
-        self.do_vf_test = do_vf_test
+        self.strategy = strategy
 
         self.age = base_age
         self.next_test = None
@@ -140,8 +139,8 @@ class Human(object):
         self.trt_len = 0
         self.trt_eff = None
         self.old_fx = None
-        self.ost_test = None
-        self.vfa_test = None
+        self.ost_result = None
+        self.vfa_result = None
         self.fx_rate =  'none'
         self.drug_end = 0
         self.drug_holiday_end = 0
@@ -159,8 +158,8 @@ class Human(object):
                 self.age, self.next_test, self.drug_end, self.last_hip, self.last_vf, self.last_wf, self.inc_utils, self.acc_utils, self.inc_cost, self.acc_cost, ost, vfa, self.fx)
 
         if self.fx != 'death':
-            return "{}   trt={:6},len={:2},eff={:6}, holiday_end={:5} ost_test={:6}, vfa_test={:6}, hip_cnt={:2}, vf_cnt={:2}, wf_cnt={:2}, trt_cnt={:4}, fx_rate={:6}".format(
-                header, str(self.trt),self.trt_len, str(self.trt_eff), self.drug_holiday_end, self.ost_test, self.vfa_test, self.hip_cnt, self.vf_cnt, self.wf_cnt, self.trt_cnt, self.fx_rate)
+            return "{}   trt={:6},len={:2},eff={:6}, holiday_end={:5} ost_result={:6}, vfa_result={:6}, hip_cnt={:2}, vf_cnt={:2}, wf_cnt={:2}, trt_cnt={:4}, fx_rate={:6}".format(
+                header, str(self.trt),self.trt_len, str(self.trt_eff), self.drug_holiday_end, self.ost_result, self.vfa_result, self.hip_cnt, self.vf_cnt, self.wf_cnt, self.trt_cnt, self.fx_rate)
         else:
             return header
 
@@ -235,32 +234,61 @@ class Human(object):
 
 
 
-    def ost_result(self):
+    def do_ost_test(self):
         if self.ost:
             #  if has ost, OST_SENSI chance positve test result.
             if random.random() < OST_SENSI:
             #if random.random() < ost_sensi(self.age):
+                self.ost_result = 'Pos'
                 return True
             else:
+                self.ost_result = 'Neg'
                 return False
         else:
             if random.random() > OST_SPEC:
                 #if random.random() > ost_spec(self.age):
+                self.ost_result = 'Pos'
                 trt = True
             else:
+                self.ost_result = 'Neg'
                 trt = False
 
 
     def strategy1_no_screening(self):
         return False
+    def strategy2(self):
+        self.do_ost_test()
+        if self.ost_result == ''
+
+    def strategy3(self):
+        return False
 
 
-    def lab_test_strategy(self, strategy):
+    def lab_test(self, strategy):
         if self.fx != 'no_fx':
             trt = True
-        self.ost_test = None
-        self.vfa_test = None
-        if
+        self.ost_result = None
+        self.vfa_result = None
+
+        do_test = False
+        if self.next_test is None:
+            do_test = True
+            self.next_test = self.age + self.test_freq
+        if self.age == self.next_test:
+            do_test = True
+            self.next_test = self.age + self.test_freq
+
+        strategy_result = False
+        if do_test:
+            if strategy == 'no screening':
+                strategy_result = self.strategy1_no_screening()
+            elif strategy == 'strategy2':
+                strategy_result = self.strategy2()
+            elif strategy == 'strategy3':
+                strategy_result = self.strategy3()
+
+
+
 
         if trt:
             self.drug_end = max(self.drug_end, self.age + DRUG_PERIOD)
@@ -273,73 +301,6 @@ class Human(object):
 
 
 
-    def lab_test(self):
-        # #TODO delete
-        # print self.fx
-
-
-
-
-
-        do_test = False
-        if self.next_test is None:
-            do_test = True
-            self.next_test = self.age + self.test_freq
-        if self.age == self.next_test:
-            do_test = True
-            self.next_test = self.age + self.test_freq
-
-        trt = False
-        # if there is fx, ost is test is always positive, and therefore get treatment
-        #TODO: enable this
-        if self.fx != 'no_fx':
-            trt = True
-        self.ost_test = None
-        self.vfa_test = None
-
-        if do_test:
-            # if no fracture.
-            if self.do_ost_test:
-
-                if self.do_vf_test and self.ost_test == 'Neg':
-                    if self.vfa:
-                        if random.random() < VFA_SENSI:
-                            self.vfa_test = 'Pos'
-                            trt = True
-                        else:
-                            self.vfa_test = 'Neg'
-                            trt = False
-                    else:
-                        if random.random() > VFA_SPEC:
-                            self.vfa_test = 'Pos'
-                            trt = True
-                        else:
-                            self.vfa_test = 'Neg'
-                            trt = False
-        #TODO: with drug holiday
-        # if trt:
-        #     self.drug_end = max(self.drug_end, self.age + DRUG_PERIOD)
-        if self.age > self.drug_holiday_end:
-            if trt:
-                self.drug_end = self.age + DRUG_PERIOD
-                self.drug_holiday_end = self.age + DRUG_PERIOD + DRUG_HOLIDAY
-
-
-        self.trt = self.age < self.drug_end
-        if self.trt:
-            self.trt_len += 1
-        # TODO: effect until drug_holiday_end
-        # else:
-        #     self.trt_len = 0
-        if self.age >= self.drug_holiday_end:
-            self.trt_len = 0
-
-
-        # if self.trt is None:
-        #     raise Exception("trt cannot be None")
-        # if self.do_ost_test and (self.ost_test is None):
-        #     raise Exception("everyone should have done OST TEST")
-
     def get_status_str(self):
         if self.trt_len > EFF_DELAY:
             self.trt_eff = True
@@ -348,9 +309,9 @@ class Human(object):
         return to_status_str(self.ost, self.vfa, self.trt_eff)
 
     def add_cost(self):
-        if self.ost_test is not None:
+        if self.ost_result is not None:
             self.inc_cost += ost_cost()
-        if self.vfa_test is not None:
+        if self.vfa_result is not None:
             self.inc_cost += vf_cost()
         if self.trt:
             self.inc_cost += drug_cost()
@@ -441,70 +402,70 @@ class Human(object):
 
 
 
-def do_group(sample_num=None, base_age=None, test_freq=None, do_ost_test=None, do_vf_test=None):
-    print "sample_num={}, base_age={}, test_freq={}, do_ost_test={}, do_vf_test={}".format(sample_num, base_age, test_freq, do_ost_test, do_vf_test)
-    counter_health = Counter()
-    counter_hip = Counter()
-    counter_vf = Counter()
-    counter_wf = Counter()
-    total_cost = 0
-    total_utils = 0
-    total_hip = 0
-    total_vf = 0
-    total_wf = 0
-    total_trt = 0
-    total_age = 0
-    for i in xrange(sample_num):
-        h = Human(base_age=base_age, test_freq=test_freq, do_ost_test=do_ost_test, do_vf_test=do_vf_test)
-        h.do_life()
-        total_cost += h.acc_cost
-        total_utils += h.acc_utils
-        total_hip += h.hip_cnt
-        total_vf += h.vf_cnt
-        total_wf += h.wf_cnt
-        total_trt += h.trt_cnt
-        total_age += h.age
-        counter_hip[h.hip_cnt] +=1
-        counter_vf[h.vf_cnt] +=1
-        counter_wf[h.wf_cnt] +=1
-        if (not h.ost) and (not h.vfa):
-            counter_health[('no_ost_no_vfa')] += 1
-        elif (not h.ost) and h.vfa:
-            counter_health[('no_ost_vfa')] += 1
-        elif h.ost and (not h.vfa):
-            counter_health[('ost_no_vfa')] += 1
-        elif h.ost and h.vfa:
-            counter_health[('ost_vfa')] += 1
-        if i%1000 == 0:
-            sys.stdout.write('.')
-            sys.stdout.flush()
+# def do_group(sample_num=None, base_age=None, test_freq=None, do_ost_test=None, do_vf_test=None):
+#     print "sample_num={}, base_age={}, test_freq={}, do_ost_test={}, do_vf_test={}".format(sample_num, base_age, test_freq, do_ost_test, do_vf_test)
+#     counter_health = Counter()
+#     counter_hip = Counter()
+#     counter_vf = Counter()
+#     counter_wf = Counter()
+#     total_cost = 0
+#     total_utils = 0
+#     total_hip = 0
+#     total_vf = 0
+#     total_wf = 0
+#     total_trt = 0
+#     total_age = 0
+#     for i in xrange(sample_num):
+#         h = Human(base_age=base_age, test_freq=test_freq, do_ost_test=do_ost_test, do_vf_test=do_vf_test)
+#         h.do_life()
+#         total_cost += h.acc_cost
+#         total_utils += h.acc_utils
+#         total_hip += h.hip_cnt
+#         total_vf += h.vf_cnt
+#         total_wf += h.wf_cnt
+#         total_trt += h.trt_cnt
+#         total_age += h.age
+#         counter_hip[h.hip_cnt] +=1
+#         counter_vf[h.vf_cnt] +=1
+#         counter_wf[h.wf_cnt] +=1
+#         if (not h.ost) and (not h.vfa):
+#             counter_health[('no_ost_no_vfa')] += 1
+#         elif (not h.ost) and h.vfa:
+#             counter_health[('no_ost_vfa')] += 1
+#         elif h.ost and (not h.vfa):
+#             counter_health[('ost_no_vfa')] += 1
+#         elif h.ost and h.vfa:
+#             counter_health[('ost_vfa')] += 1
+#         if i%1000 == 0:
+#             sys.stdout.write('.')
+#             sys.stdout.flush()
 
 
-    ave_cost = total_cost/sample_num
-    ave_utils = total_utils/sample_num/2
-    ave_age = total_age/sample_num
-    health_str = "no_ost_no_vfa={:8d}, no_ost_vfa={:8d}, ost_no_vfa={:8d}, ost_vfa={:8d}".format(counter_health['no_ost_no_vfa'], counter_health['no_ost_vfa'], counter_health['ost_no_vfa'], counter_health['ost_vfa'])
-    main_result = "samples={}, test_freq={}, do_ost_test={:8s}, do_vf_test={:8s}, ave_cost={:14.4f}, ave_utils={:14.4f}, total_hip={:8}, total_vf={:8}, total_wf={:8}, total_trt={:8}, ave_age={:4}".format(sample_num, test_freq, str(do_ost_test), str(do_vf_test), ave_cost, ave_utils, total_hip, total_vf, total_wf, total_trt, ave_age)
-    print ""
-    print "{:230s} {}".format(main_result, health_str)
-    print counter_health
-    print 'hip cnt {}'.format(counter_hip)
-    print 'vf  cnt {}'.format(counter_vf)
-    print 'wf  cnt {}'.format(counter_wf)
-    print ""
+#     ave_cost = total_cost/sample_num
+#     ave_utils = total_utils/sample_num/2
+#     ave_age = total_age/sample_num
+#     health_str = "no_ost_no_vfa={:8d}, no_ost_vfa={:8d}, ost_no_vfa={:8d}, ost_vfa={:8d}".format(counter_health['no_ost_no_vfa'], counter_health['no_ost_vfa'], counter_health['ost_no_vfa'], counter_health['ost_vfa'])
+#     main_result = "samples={}, test_freq={}, do_ost_test={:8s}, do_vf_test={:8s}, ave_cost={:14.4f}, ave_utils={:14.4f}, total_hip={:8}, total_vf={:8}, total_wf={:8}, total_trt={:8}, ave_age={:4}".format(sample_num, test_freq, str(do_ost_test), str(do_vf_test), ave_cost, ave_utils, total_hip, total_vf, total_wf, total_trt, ave_age)
+#     print ""
+#     print "{:230s} {}".format(main_result, health_str)
+#     print counter_health
+#     print 'hip cnt {}'.format(counter_hip)
+#     print 'vf  cnt {}'.format(counter_vf)
+#     print 'wf  cnt {}'.format(counter_wf)
+#     print ""
 
 
 
-def run_groups():
-    SAMPLE_NUM = 1000*490
-    do_group(SAMPLE_NUM, test_freq=5, do_ost_test=False, do_vf_test=False)
-    do_group(SAMPLE_NUM, test_freq=5, do_ost_test=True, do_vf_test=False)
-    do_group(SAMPLE_NUM, test_freq=5, do_ost_test=True, do_vf_test=True)
+# def run_groups():
+#     SAMPLE_NUM = 1000*490
+#     do_group(SAMPLE_NUM, test_freq=5, do_ost_test=False, do_vf_test=False)
+#     do_group(SAMPLE_NUM, test_freq=5, do_ost_test=True, do_vf_test=False)
+#     do_group(SAMPLE_NUM, test_freq=5, do_ost_test=True, do_vf_test=True)
 
-def run_one_person(base_age=None, test_freq=None, do_ost_test=None, do_vf_test=None):
-    print "base_age={}, test_freq={}, do_ost_test={}, do_vf_test={}".format(base_age, test_freq, do_ost_test, do_vf_test)
-    h = Human(base_age=base_age, test_freq=test_freq, do_ost_test=do_ost_test, do_vf_test=do_vf_test)
-    h.do_life(prt=True)
+# def run_one_person(base_age=None, test_freq=None, do_ost_test=None, do_vf_test=None):
+#     print "base_age={}, test_freq={}, do_ost_test={}, do_vf_test={}".format(base_age, test_freq, do_ost_test, do_vf_test)
+#     h = Human(base_age=base_age, test_freq=test_freq, do_ost_test=do_ost_test, do_vf_test=do_vf_test)
+#     h.do_life(prt=True)
 
 
 
